3b184fef1b20ebbe51331d12072e434d8bc73d15
Nagy Boldizsar <nbozsi4@gmail.com>
2025-05-08 22:49:29 +0200

    random forest try, too slow yet

--- random_forest_model.py
+++ random_forest_model.py
@@ -4,7 +4,37 @@ from preprocess_dataset_for_training import create_training_data
 import polars as pl
 import time
 
-joined_df = pl.read_parquet("data/joined_df.parquet")
+keep_cols = (
+    "Időpont",
+    "Negatív Mérlegköri kiegyenlítő energia egységára (HUF/kWh)",
+    "Pozitív Mérlegköri kiegyenlítő energia egységára (HUF/kWh)",
+    "Rendszer-irány (kWh)",
+    "Naperőművek becsült termelése (aktuális)",
+    "Naperőművek becsült termelése (intraday)",
+    "Naperőművek becsült termelése (dayahead)",
+    "HU-UK",
+    "HU-SK",
+    # "HU-SI", too many NaN values
+    "HU-RS",
+    "HU-AT",
+    "HU-HR",
+    "HU-RO",
+    "HU-AT menetrend",
+    "HU-HR menetrend",
+    # "HU-SI menetrend (RIR NT)", too many NaN values
+    "HU-SK menetrend",
+    "HU-RS menetrend",
+    "HU-UK menetrend",
+    "HU-RO menetrend",
+    "Bruttó terv erőművi termelés",
+    "Bruttó tény erőművi termelés",
+    "Bruttó hitelesített rendszerterhelés tény",
+    "Bruttó rendszerterhelés becslés (dayahead)",
+    "Szélerőművek becsült termelése (aktuális)",
+    "Szélerőművek becsült termelése (dayahead)",
+    "Szélerőművek becsült termelése (intraday)",
+)
+joined_df = pl.read_parquet("data/joined_df.parquet").select(keep_cols)
 X, y = create_training_data(joined_df)
 
 
@@ -18,7 +48,7 @@ cv = KFold(n_splits=2, shuffle=True)
 for target in y.columns:
 
     grid_search = GridSearchCV(
-        estimator=RandomForestRegressor(),
+        estimator=RandomForestRegressor(n_jobs=-1),
         param_grid=param_grid,
         return_train_score=True,
         cv=cv,
