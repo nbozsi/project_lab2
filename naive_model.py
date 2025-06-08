69dde7886bd53f3963da49bde107f2345c95d21e
Nagy Boldizsar <nbozsi4@gmail.com>
2025-06-08 22:08:44 +0200

    better x ticks

--- naive_model.py
+++ naive_model.py
@@ -1,5 +1,4 @@
 import polars as pl
-
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error
 import altair as alt
 
@@ -14,17 +13,10 @@ TARGET_COLS = (
 )
 
 
-def lag_to_time_label(lag: int) -> str:
-    minutes = lag * 15
-    hours = minutes // 60
-    mins = minutes % 60
-    return f"{hours}:{mins:02d}"
-
-
 def lag_chart(df, colors=["red", "green"], width=800, height=400):
     # Base encodings
-    base = alt.Chart(df.with_columns((pl.col("lag").map_elements(lag_to_time_label, return_dtype=str)))).encode(
-        x=alt.X("lag", axis=alt.Axis(title="Lag", labelAngle=0, labelFontSize=14, titleFontSize=14)),
+    base = alt.Chart(df).encode(
+        x=alt.X("lag:T", axis=alt.Axis(title="Lag", labelAngle=0, labelFontSize=14, titleFontSize=14, tickCount=4, format="%H:%M")),
         y=alt.Y("error:Q", axis=alt.Axis(title="Error", labelFontSize=14, titleFontSize=14)),
         color=alt.Color("target", scale=alt.Scale(range=colors)).legend(
             orient="bottom", labelFontSize=11, titleFontSize=13, labelLimit=310
@@ -61,8 +53,13 @@ for lag in TIMELAGS:
             }
         )
 
-results = pl.from_dicts(results).unpivot(["mae", "rmse"], index=["target", "lag"], variable_name="error measure", value_name="error")
-
+results = (
+    pl.from_dicts(results)
+    .unpivot(["mae", "rmse"], index=["target", "lag"], variable_name="error measure", value_name="error")
+    .with_columns(
+        (pl.col("lag") * 15 * 60 * 1000).cast(pl.Datetime("ms")),
+    )
+)
 
 chart = lag_chart(results.filter(pl.col("target") == "System Direction (kWh)"), ["blue"])
 chart.save("figures/naive_model_performance_system_direction.png")
