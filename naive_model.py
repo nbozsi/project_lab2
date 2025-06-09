2a5c63c892f674c082b8c9cd5653b2b1c7d14fdd
Nagy Boldizsar <nbozsi4@gmail.com>
2025-06-09 12:26:22 +0200

    r2 score added

--- naive_model.py
+++ naive_model.py
@@ -1,5 +1,5 @@
 import polars as pl
-from sklearn.metrics import mean_absolute_error, root_mean_squared_error
+from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
 import altair as alt
 
 
@@ -19,7 +19,7 @@ def lag_chart(df, colors=["red", "green"], width=800, height=400):
         x=alt.X("lag:T", axis=alt.Axis(title="Lag", labelAngle=0, labelFontSize=14, titleFontSize=14, tickCount=4, format="%H:%M")),
         y=alt.Y("error:Q", axis=alt.Axis(title="Error", labelFontSize=14, titleFontSize=14)),
         color=alt.Color("target", scale=alt.Scale(range=colors)).legend(
-            orient="bottom", labelFontSize=11, titleFontSize=13, labelLimit=310
+            orient="bottom", direction="vertical", labelFontSize=11, titleFontSize=13, labelLimit=400
         ),
         strokeDash=alt.StrokeDash("error measure").legend(orient="bottom", labelFontSize=11, titleFontSize=13),
     )
@@ -50,19 +50,26 @@ for lag in TIMELAGS:
                 "lag": lag,
                 "mae": mean_absolute_error(y_true, y_pred),
                 "rmse": root_mean_squared_error(y_true, y_pred),
+                "R2": r2_score(y_true, y_pred),
             }
         )
 
 results = (
     pl.from_dicts(results)
-    .unpivot(["mae", "rmse"], index=["target", "lag"], variable_name="error measure", value_name="error")
+    .unpivot(["mae", "rmse", "R2"], index=["target", "lag"], variable_name="error measure", value_name="error")
     .with_columns(
         (pl.col("lag") * 15 * 60 * 1000).cast(pl.Datetime("ms")),
     )
 )
 
-chart = lag_chart(results.filter(pl.col("target") == "System Direction (kWh)"), ["blue"])
-chart.save("figures/naive_model_performance_system_direction.png")
+sys_dir = results.filter((pl.col("target") == "System Direction (kWh)"))
+unit_prices = results.filter((pl.col("target") != "System Direction (kWh)"))
 
-chart = lag_chart(results.filter(pl.col("target") != "System Direction (kWh)"))
-chart.save("figures/naive_model_performance_unit_price.png")
+chart = lag_chart(sys_dir.filter(pl.col("error measure").is_in(("rmse", "mae"))), ["blue"])
+chart.save("figures/naive_model_rmse_system_direction.png")
+
+chart = lag_chart(unit_prices.filter(pl.col("error measure").is_in(("rmse", "mae"))))
+chart.save("figures/naive_model_rmse_unit_price.png")
+
+chart = lag_chart(results.filter(pl.col("error measure") == "R2"), ["red", "green", "blue"])
+chart.save("figures/naive_model_R2.png")
