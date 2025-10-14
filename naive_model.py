5965184f2f820b57769f71c65d22df1e3869e7fc
Nagy Boldizsar <nbozsi4@gmail.com>
2025-10-14 14:44:42 +0200

    added new compare figures

--- naive_model.py
+++ naive_model.py
@@ -13,7 +13,7 @@ TARGET_COLS = (
 )
 
 
-def lag_chart(df, colors=["red", "green"], width=800, height=400):
+def lag_chart(df, colors=["red", "green"], model_name=None, width=800, height=400):
     # Base encodings
     if "model" in df.columns:
         base = alt.Chart(df).encode(
@@ -87,34 +87,48 @@ chart.save("figures/naive_model_rmse_unit_price.png")
 chart = lag_chart(naive_results.filter(pl.col("error measure") == "R²"), ["red", "green", "blue"])
 chart.save("figures/naive_model_R2.png")
 
-results = pl.read_parquet("xgboost_separate_results.parquet")
 
-results = results.with_columns(
-    pl.col("Target").str.strip_chars_end("t+1234567890min").str.strip_suffix("_"),
-    (pl.col("Target").str.extract(r"_t\+(\d*)min").cast(pl.Int32) * 60 * 1000).cast(pl.Datetime("ms")).alias("lag"),
-).unpivot(["MAE", "RMSE", "R²"], index=["Target", "lag"], variable_name="error measure", value_name="error")
+def unpivot_errors(df):
+    return df.with_columns(
+        pl.col("Target").str.strip_chars_end("t+1234567890min").str.strip_suffix("_"),
+        (pl.col("Target").str.extract(r"_t\+(\d*)min").cast(pl.Int32) * 60 * 1000).cast(pl.Datetime("ms")).alias("lag"),
+    ).unpivot(["MAE", "RMSE", "R²"], index=["Target", "lag"], variable_name="error measure", value_name="error")
 
-model_name = "xgboost"
-compare = pl.concat(
-    (
-        results.with_columns(model=pl.lit(model_name)),
-        naive_results.with_columns(model=pl.lit("Naive model")),
-    ),
-    how="vertical",
-)
-print(compare)
-sys_dir = compare.filter((pl.col("Target") == "System Direction (kWh)"))
-unit_prices = compare.filter((pl.col("Target") != "System Direction (kWh)"))
 
-chart = lag_chart(sys_dir.filter(pl.col("error measure") == "MAE"), ["blue"])
-chart.save(f"figures/{model_name}_mae_system_direction.png")
-chart = lag_chart(sys_dir.filter(pl.col("error measure") == "RMSE"), ["blue"])
-chart.save(f"figures/{model_name}_rmse_system_direction.png")
+def compare_with_naive(results, naive_results, model_name):
+    compare = pl.concat(
+        (
+            results.with_columns(model=pl.lit(model_name)),
+            naive_results.with_columns(model=pl.lit("Naive model")),
+        ),
+        how="vertical",
+    )
 
-chart = lag_chart(unit_prices.filter(pl.col("error measure") == "MAE"))
-chart.save(f"figures/{model_name}_mae_unit_price.png")
-chart = lag_chart(unit_prices.filter(pl.col("error measure") == "RMSE"))
-chart.save(f"figures/{model_name}_rmse_unit_price.png")
+    sys_dir = compare.filter((pl.col("Target") == "System Direction (kWh)"))
+    unit_prices = compare.filter((pl.col("Target") != "System Direction (kWh)"))
 
-chart = lag_chart(compare.filter(pl.col("error measure") == "R²"), ["red", "green", "blue"])
-chart.save(f"figures/{model_name}_R2.png")
+    chart = lag_chart(sys_dir.filter(pl.col("error measure") == "MAE"), ["blue"], model_name=model_name)
+    chart.save(f"figures/{model_name}_mae_system_direction.png")
+    chart = lag_chart(sys_dir.filter(pl.col("error measure") == "RMSE"), ["blue"], model_name=model_name)
+    chart.save(f"figures/{model_name}_rmse_system_direction.png")
+
+    chart = lag_chart(unit_prices.filter(pl.col("error measure") == "MAE"), model_name=model_name)
+    chart.save(f"figures/{model_name}_mae_unit_price.png")
+    chart = lag_chart(unit_prices.filter(pl.col("error measure") == "RMSE"), model_name=model_name)
+    chart.save(f"figures/{model_name}_rmse_unit_price.png")
+
+    chart = lag_chart(compare.filter(pl.col("error measure") == "R²"), ["red", "green", "blue"], model_name=model_name)
+    chart.save(f"figures/{model_name}_R2.png")
+
+
+results = unpivot_errors(pl.read_parquet("xgboost_separate_results.parquet"))
+compare_with_naive(results, naive_results, "xgboost")
+
+results = unpivot_errors(pl.read_csv("evaluation_metrics_joined_df_with_temp.csv"))
+compare_with_naive(results, naive_results, "NN_with_temp")
+
+results = unpivot_errors(pl.read_csv("evaluation_metrics_joined_df_with_weather.csv"))
+compare_with_naive(results, naive_results, "NN_with_weather")
+
+results = unpivot_errors(pl.read_csv("evaluation_metrics_joined_df.csv"))
+compare_with_naive(results, naive_results, "NN")
